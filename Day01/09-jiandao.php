47708af23e0d585a15f3dc583a8aaec34f921f06
myvipbackup2 <lzxvipmail@vip.qq.com>
2016-12-08 21:27:47 +0800

    猜拳游戏

---
+++ Day01/09-jiandao.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lzx
+ * Date: 2016/12/8 0008
+ * Time: 21:24
+ */
+
+//猜拳
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>猜拳游戏</title>
+</head>
+<body>
+    <div id="container">
+
+    </div>
+</body>
+</html>
