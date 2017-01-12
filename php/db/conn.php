b03d86859913d3409aaa6292ffe6c7d2d857cec5
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-01-12 21:45:35 +0800

    留言板update

--- php/db/conn.php
+++ php/db/conn.php
@@ -4,7 +4,7 @@
 	
 
 	//1:打开数据库连接
-	$link=@mysqli_connect('localhost','root','') or die('数据库连接失败');
+	$link=@mysqli_connect('localhost','root','root') or die('数据库连接失败');
 	//2:选择数据库
 	@mysqli_select_db($link,'blog6') or die('选择数据库失败');
 	//3:设置传输编码
--- php/messageBoard/all.php
+++ php/messageBoard/all.php
@@ -12,16 +12,24 @@ include "conn.php";
 if (isset($_GET['wid'])) {
 
     $wid = $_GET['wid'];
+    if (!$wid) {
+        echo "<script>alert('参数出错！')</script>";
+        echo "<script>window.location.href='index.php'</script>";
+    }
     $sql = "update blog set hits=hits+1 where wid='$wid'";
-    $query = mysqli_query($link,$sql);
-    if ($query){
+    $query = mysqli_query($link, $sql);
+    if ($query) {
         $sql = "select * from blog where wid='$wid'";
         $query = mysqli_query($link, $sql);
         $rs = mysqli_fetch_array($query);
-    }else{
-        echo "";
+    } else {
+        echo "<script>alert('内部服务出错！')</script>";
+        echo "<script>window.location.href='index.php'</script>";
     }
 
+} else {
+    echo "<script>alert('参数出错！')</script>";
+    echo "<script>window.location.href='index.php'</script>";
 }
 
 
@@ -39,22 +47,60 @@ if (isset($_GET['wid'])) {
 </head>
 <body>
 
-<div style="width: 800px; height: 600px; margin: 20px auto; " class="container">
-    <a href="index.php" style="text-decoration: none;display: block; background-color:cornflowerblue;color: #ffffff; width: 100px;height: 50px; line-height: 50px;text-align: center;border-radius: 10px">
+<div style="width: 800px;margin: 20px auto; " class="container">
+    <a href="index.php"
+       style="text-decoration: none;display: block; background-color:cornflowerblue;color: #ffffff; width: 100px;height: 50px; line-height: 50px;text-align: center;border-radius: 10px">
         &lt; 返回主页
     </a>
     <h3 style="text-align: center;font-size: 30px">标题：<a href="#"><?php echo $rs['title'] ?></a></h3>
     <span style="color: #ff0000;">访问量：<?php echo $rs['hits'] ?></span>
     <br><br>
     内容：<p style="font-size: 20px;background-color:darkgray;"><?php echo $rs['content'] ?></p>
-    <form action="all.php" method="post">
 
-        <textarea style="width: 80%;margin: 0 auto; display: block; " name="plcon" id="" cols="30" rows="10"></textarea>
+    <?php
+
+    if (isset($_POST['sub'])) {
+
+        $plcon = $_POST['plcon'];
+        $wid = $_GET['wid'];
+        $uid = $_COOKIE['id'];
+
+        $sql = "insert into pl(pid,pcon,ptime,wid,uid) VALUES(NULL,'$plcon',now(),'$wid','$uid')";
+        $query = mysqli_query($link, $sql);
+        if ($query) {
+            header("location:all.php?wid=$wid");
+        } else {
+            echo "<script>alert('评论出错！')</script>";
+            echo "<script>window.location.href='all.php?wid=$wid'</script>";
+        }
+
+    }
+
+    ?>
+
+    <form action="all.php?wid=<?php echo $rs['wid'] ?>" method="post">
+
+        <textarea style="resize:none;width: 80%;margin: 0 auto; display: block; " name="plcon" id="" cols="30"
+                  rows="10"></textarea>
         <br>
         <input style="display: block;margin: 0 auto;width: 100px;height: 40px;" type="submit" name="sub" value="评论">
 
     </form>
 
+    <?php
+
+    $sql = "select * from pl ,user where user.uid= pl.uid and wid='$wid'";
+    $query = mysqli_query($link, $sql);
+    while ($rs = mysqli_fetch_array($query)) {
+        ?>
+        <div style="background-color:darkseagreen;">
+            <p>评论内容：<?php echo $rs['pcon'] ?></p>
+            <span>评论人：<?php echo $rs['uname'] ?></span>
+            <span>评论时间：<?php echo $rs['ptime']?></span>
+        </div>
+        <?php
+    }
+    ?>
 </div>
 
 </body>
