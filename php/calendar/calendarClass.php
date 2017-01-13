addc88b9b3fd2e50f93ceaecabeacb2aeb75a4b0
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-01-13 20:53:14 +0800

    update

--- php/calendar/calendarClass.php
+++ php/calendar/calendarClass.php
@@ -8,11 +8,26 @@
  */
 class calendarClass
 {
+    private $year;
+    private $month;
+    private $days;
+    private $weeknum;
+
+    public function __construct()
+    {
+        $this->year = $_GET['year'] ? $_GET['year'] : date('Y');
+        $this->month = $_GET['month'] ? $_GET['month'] : date('m');
+        $this->days = date('t', mktime(0, 0, 0, $this->month, 1, $this->year));
+        $this->weeknum = date('w', mktime(0, 0, 0, 2, 1, $this->year));
+    }
+
 
     public function out()
     {
         echo "<table align='center' >";
+        $this->changeDays();
         $this->weekList();
+        $this->daysList();
         echo "</table>";
     }
 
@@ -30,6 +45,22 @@ class calendarClass
     public function daysList()
     {
         echo "<tr>";
+//        先打空格 当前年当前月的1号是周几
+        for ($j = 0; $j < $this->weeknum; $j++) {
+            echo "<td>" . "&nbsp;" . "</td>";
+        }
+//        输出日期
+        for ($k = 1; $k <= $this->days; $k++) {
+            $j++;
+            if ($k == date('d')) {
+                echo "<td class='fontb'>" . $k . "</td>";
+            } else {
+                echo "<td>" . $k . "</td>";
+            }
+            if ($j % 7 == 0) {
+                echo "</tr><tr>";
+            }
+        }
 
         echo "</tr>";
     }
